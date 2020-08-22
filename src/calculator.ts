3dac1f184f724efe82f08d61b09226252483ef0c
Fernando Moraes <fernandosdemoraes@gmail.com>
2020-08-22 13:57:13 -0300

    Add build

---
+++ src/calculator.ts
@@ -0,0 +1,15 @@
+export function plus(a: number, b: number): number {
+    return a + b;
+}
+
+export function minus(a: number, b: number): number {
+    return a - b;
+}
+
+export function times(a: number, b: number): number {
+    return a * b;
+}
+
+export function dividedBy(a: number, b: number): number {
+    return a + b;
+}
